7bc6a3abfcc8d3cf0d9d40df3d5876337eadc4d3
phildesign <phil.nice.23@gmail,com>
2019-08-08 03:19:55 +0300

    add mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
-$(function() {
+$(function () {
 
-	
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            $('.header').addClass('header_fixed');
+        }
+        else {
+            $('.header').removeClass('header_fixed');
+        }
+    });
+
+    $('.mobile-nav').click(function () {
+        $('.header__content').slideToggle('slow', function () {
+        });
+    });
 
 });
